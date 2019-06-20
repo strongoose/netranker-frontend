d0a342b8923e29e1542d9f37ddebbd1a89b25461
Daniel Strong <dstrong@glyx.co.uk>
2019-06-20 21:28:03 +0100

    Put cards into flexboxes and space nicely

--- src/Pairing.js
+++ src/Pairing.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
 function Card(props) {
-  return <input
-    type='image'
+  return <img
     alt={props.name}
     src={props.src}>
-  </input>
+  </img>
 }
 
 function Pairing(props) {
-  return <div>
-    <Card
-      name='Parasite'
-      src='https://netrunnerdb.com/card_image/01012.png'
-    />
-    <Card
-      name='Temüjin Contract'
-      src='http://www.cardgamedb.com/forums/uploads/an/med_ADN37_26.png'
-    />
-   </div>
+  return <div className="container">
+    <div className="d-flex mt-5 align-items-center justify-content-center">
+      <h1>Pick the best of</h1>
+    </div>
+    <div className="d-flex mt-5 align-items-center justify-content-center">
+      <Card
+        name='Parasite'
+        // src='https://netrunnerdb.com/card_image/01012.png'
+        src='http://localhost:8000/01012.png'
+      />
+    <div className="m-5"><h2>vs</h2></div>
+      <Card
+        name='Temüjin Contract'
+        // src='http://www.cardgamedb.com/forums/uploads/an/med_ADN37_26.png'
+        src='http://localhost:8000/11026.png'
+      />
+    </div>
+  </div>
 }
 
 export default Pairing;
